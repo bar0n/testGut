348697cdbfe2e4bc4f2ad73111300b27fb723385
dbaron <dbaron@intersog.com>
2018-11-13 06:33:30 -0600

    plugin

--- src/main/java/com/hillel/Main.java
+++ src/main/java/com/hillel/Main.java
@@ -1,7 +1,31 @@
 package com.hillel;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
     }
 }
+
+/*
+class Shape {
+    public void draw(Graphics g, int x, int y) {}
+}
+class Circle extends Shape {
+    public void draw(Graphics g, float x, float y) {}
+    // Circle.draw overloads the inheried Shape.draw
+    // -- argument types for 'x' and 'y' are different here.
+}
+
+class ShapeFactory {
+    public Shape newShape(Integer i) {
+        return new Shape();
+    }
+}
+class CircleFactory extends ShapeFactory {
+    public Circle newShape(Number i) {
+        // your code from the source file
+        return new Circle();
+    }
+}*/
