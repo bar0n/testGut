fb22fecc2afb9d26b3f8e457e1023de63b120bc0
dbaron <dbaron@intersog.com>
2018-11-13 06:42:37 -0600

    maven-jar-plugin

--- src/main/java/com/hillel/Main.java
+++ src/main/java/com/hillel/Main.java
@@ -4,7 +4,8 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        String x = "Hello World!!!";
+        System.out.println(x);
     }
 }
 
