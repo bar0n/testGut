01af47ef57520c2e2d0c8a69ac5a50ee71d0e908
dbaron <dbaron@intersog.com>
2018-11-14 04:43:30 -0600

    input int

--- src/main/java/com/hillel/Main.java
+++ src/main/java/com/hillel/Main.java
@@ -1,12 +1,31 @@
 package com.hillel;
 
-import java.awt.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String x = "Hello World!!!";
         System.out.println(x);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter integer: ");
+        int i = scanner.nextInt();
+        System.out.println(i);
+
+        System.out.print("Enter first dimension: ");
+        Scanner scanner2 = new Scanner(System.in);
+        int dim1 = scanner2.nextInt();
+
+        System.out.println(dim1);
+
+        // Arrays.copyOf()
     }
+}
+
+enum Size {
+    SX,
+    XL,
+    XXL
 }
 
 /*
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,13 +1,23 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class MyTest {
 
     @Test
     public void multiplicationOfZeroIntegersShouldReturnZero() {
+        //assertEquals(1,1)
+        //assertTrue(Arrays.equals(expectedResult, result));
        /* MyClass tester = new MyClass(); // MyClass is tested
         // assert statements
-        assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
+        assertEquals("10 x 0 must be 0", 0, tester.multiply(10, 0), );
         assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
         assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");*/
+
+        assertEquals("0 x 0 must be 0",0, 0 );
+        //assertEquals("0 x 0 must be 0",0, 1 );
     }
 }
